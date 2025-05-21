bdfcb39272c8e8a8b69a4d2510fe7dc0b8240ecc
Mamoon Idrees <mamoon.idrees5@gmail.com>
2025-05-22 09:48:00 +1000

    Create app.py
    app.py commit

---
+++ app/app.py
@@ -0,0 +1,11 @@
+# Sample Flask application for CI/CD pipeline demo
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello from GitHub Actions + Terraform + Docker + AWS!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
